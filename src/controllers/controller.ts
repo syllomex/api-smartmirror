087322c30583cd5077ea4a9124c472bb6d400509
Leonardo Santos <leonardojdossantos@gmail.com>
2021-09-14 20:04:27 -0300

    chore: add controller factory

---
+++ src/controllers/controller.ts
@@ -0,0 +1,31 @@
+/* eslint-disable max-classes-per-file */
+import { Request, Response, Route } from '../types/http';
+
+class BadRequest extends Error {
+  public status = 400;
+}
+
+class NotFound extends Error {
+  public status = 404;
+}
+
+class Unauthorized extends Error {
+  public status = 401;
+}
+
+class InternalServerError extends Error {
+  public status = 500;
+}
+
+const createController = async (handler: Route, req: Request, res: Response<any>) => {
+  try {
+    const response = await handler(req, res);
+    return response;
+  } catch (err) {
+    return res.status(err.status).json({ success: false, error: err, message: err.message });
+  }
+};
+
+export {
+  createController, BadRequest, NotFound, Unauthorized, InternalServerError,
+};
