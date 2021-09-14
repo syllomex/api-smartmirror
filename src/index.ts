49ae89b0f8808c05fda51e4a6bfde0a4e13d2e88
Leonardo Santos <leonardojdossantos@gmail.com>
2021-09-14 20:35:10 -0300

    fix: add middleware

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,26 @@ const app = express();
 
 connect();
 
+const isDev = process.env.NODE_ENV === 'development';
+
 app.use(
   cors({
-    origin: 'https://smartmirror-display.vercel.app',
+    origin: isDev ? 'http://localhost:3000' : 'https://smartmirror-display.vercel.app',
     credentials: true,
   }),
 );
+
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+  );
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
