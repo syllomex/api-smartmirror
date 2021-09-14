2a8261b0a73f2f965e46c4ed1cdad0b478282f22
Leonardo Santos <leonardojdossantos@gmail.com>
2021-09-14 20:08:31 -0300

    feat: add cors origin

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 connect();
 
-app.use(cors());
+app.use(cors({ origin: 'https://smartmirror-display.vercel.app' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
