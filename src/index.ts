a3c005ccda241b9048b8231ec3b1c6875a559fc8
Leonardo Santos <leonardojdossantos@gmail.com>
2021-09-14 20:59:13 -0300

    chore: add middleware

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,22 @@ connect();
 
 const isDev = process.env.NODE_ENV === 'development';
 
-app.use(
-  cors({
-    origin: isDev ? 'http://localhost:3000' : 'https://smartmirror-display.vercel.app',
-    credentials: true,
-    allowedHeaders: '*',
-  }),
-);
+// app.use(
+//   cors({
+//     origin: isDev ? 'http://localhost:3000' : 'https://smartmirror-display.vercel.app',
+//     credentials: true,
+//     allowedHeaders: '*',
+//   }),
+// );
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://smartmirror-display.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', 1);
+
+  next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
